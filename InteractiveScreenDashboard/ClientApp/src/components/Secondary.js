aa628d3aca0c85d4f762d46012601eedbdff049f
vtharaka <vtharaka@routesme.com>
2020-07-09 03:08:52 +0300

    Expandable list on secondary panel expands based on selected id

--- InteractiveScreenDashboard/ClientApp/src/components/Secondary.js
+++ InteractiveScreenDashboard/ClientApp/src/components/Secondary.js
@@ -1,12 +1,69 @@
 ﻿import React, { Component } from 'react';
+import axios from 'axios';
 import alarem from './image/alarem.png';
 import bank from './image/bank.png';
 import carnew from './image/carnew.png';
 import phone from './image/phone.png';
+import SecondaryList from './SecondaryList';
 
 export class Secondary extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+            Vehicles: [],
+            loading: true,
+            failed: false,
+            error: '',
+            selectedIndex:2
+        };
+
+    }
+
+    componentDidMount() {
+        this.populateVehicleData();
+    }
+
+    populateVehicleData() {
+        axios.get("http://localhost:55205/api/Vehicles").then(result => {
+            const response = result.data;
+            this.setState({ Vehicles: response, loading: false, failed: false, error: "" });
+        }).catch(error => {
+            this.setState({ vehicles: [], loading: false, failed: true, error: "Vehicles could not be loaded" });
+        });
+    }
+
+    toggleItem(index) {
+        this.setState({ selectedIndex: index });
+    }
+
+    renderAllVehicles(Vehicles) {
+        console.log('Selected Index :', this.state.selectedIndex)
+        return (
+            <div>
+            {
+              Vehicles.map(Vehicle => (
+                        <div onClick={(e) => this.toggleItem(Vehicle.id)}>
+                        <SecondaryList vehicle={Vehicle} index={Vehicle.id} selectedIndex={this.state.selectedIndex} />
+                        </div>
+              ))
+            }
+            </div>
+        )
+    }
+
     render() {
+
+        let content = this.state.loading ?
+            <div><br /><br /><p><em> Loading...</em> </p></div> :
+            this.state.failed ?
+                <div className="text-danger"><br /><br />
+                    <em>{this.state.error}</em>
+                </div> :
+                this.renderAllVehicles(this.state.Vehicles);
+
         return (
 
            
@@ -18,24 +75,12 @@ export class Secondary extends Component {
 
                 <div className="search-result">
                     <p>Free</p>
-                    <div className="slide-effect">
-                        <p className="location-button">Fulan Abu Flan</p>
-                        <ul className="location-detail">
-                            <li><a href="#"><div className="icon-30"><img alt="" src={alarem} className="icon" /></div> Running last 4hrs</a></li>
-                            <li><a href="#"><div className="icon-30"><img alt="" src={carnew} className="icon" /></div> Toyota Camry.2019</a></li>
-                            <li><a href="#"><div className="icon-30"><img alt="" src={phone} className="icon" /></div> +965 656552514</a></li>
-                            <li><a href="#"><div className="icon-30"><img alt="" src={bank} className="icon" /></div> Afnan</a></li>
-                        </ul>
-                    </div>
-                    <ul className="manual-menu first-sec">
-                        <li><a href="#">Fulan Abu Flan</a></li>
-                        <li><a href="#">Mohammad All</a></li>
-                        <li><a href="#">Saad Mue</a></li>
-                        <li><a href="#">Waseem Noor</a></li>
-                    </ul>
+                    {content}
+                    
 
                     <p>occupied</p>
                     <ul className="manual-menu">
+
                         <li><a href="#">Fulan Abu Flan</a></li>
                         <li><a href="#">Mohammad All</a></li>
                         <li><a href="#">Saad Mue</a></li>
--- InteractiveScreenDashboard/ClientApp/src/components/SecondaryList.js
+++ InteractiveScreenDashboard/ClientApp/src/components/SecondaryList.js
@@ -1,1 +1,21 @@
-﻿
+﻿import React, { Component } from 'react';
+import alarem from './image/alarem.png';
+import bank from './image/bank.png';
+import carnew from './image/carnew.png';
+import phone from './image/phone.png';
+
+export const SecondaryList = ({ vehicle, index, selectedIndex }) => {
+    return (
+        <div className="slide-effect" key={vehicle.id}>
+            <p className="location-button">{vehicle.id}</p>
+            {selectedIndex === index ?
+            <ul className="location-detail">
+            <li><a href="#"><div className="icon-30"><img alt="" src={alarem} className="icon" /></div> {vehicle.make}</a></li>
+            <li><a href="#"><div className="icon-30"><img alt="" src={carnew} className="icon" /></div> {vehicle.model}</a></li>
+            <li><a href="#"><div className="icon-30"><img alt="" src={phone} className="icon" /></div>  {vehicle.plate}</a></li>
+            <li><a href="#"><div className="icon-30"><img alt="" src={bank} className="icon" /></div>{vehicle.office}</a></li>
+             </ul>: ""}
+            </div>
+        )
+}
+export default SecondaryList;
