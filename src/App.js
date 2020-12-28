473761016ebc7c50738a18d9a2ff38de346fcd94
vivian tharakan <vtharaka@routesme.com>
2020-12-28 17:32:49 +0300

    Login page lazy load

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 
 
 import React, { Component, Suspense, lazy } from 'react';
-
-//import { Route } from 'react-router';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { history } from '../src/helper/history';
 import { Layout } from './components/Layout';
-import { Login } from './components/Login/Login';
 import { UsersDetail } from './components/Users/UsersDetail';
 import { PrivateRoute } from './helper/PrivateRoute';
 import Launch from './components/Launch';
@@ -14,16 +11,11 @@ import Launch from './components/Launch';
 //import { ForgotPassword } from './components/ForgotPassword';
 //import { savePassword } from './components/savePassword';
 //import { ResetPassword } from './components/ResetPassword';
-
-
-//React.lazy(async () => (await import('./foo/foo').MyComponent)
-//const PrivateRoute = lazy(() => import('./components/PrivateRoute'))
-//const Home = lazy(async () => (await import('./components/Home')));
 const ForgotPassword = lazy(() => import('./components/Password/ForgotPassword'));
 const savePassword = lazy(() => import('./components/Password/savePassword'));
 const ResetPassword = lazy(() => import('./components/Password/ResetPassword'));
 const Home = lazy(() => import('./components/Home'));
-
+const Login = lazy(() => import('./components/Login/Login'));
 
 export default class App extends Component {
     static displayName = App.name;
@@ -33,14 +25,14 @@ export default class App extends Component {
             <Layout>
                 <Router  history={history}>
                     <Switch>
+                        <Suspense fallback={<Launch/>}>
                         <Route exact path='/' component={Login} />
                         <Route exact path='/AddAccount' component={UsersDetail}/>
-                        <Suspense fallback={<Launch/>}>
                         <PrivateRoute exact path='/home' component={Home} />
                         <Route path='/forgotpassword' component={ForgotPassword} />
                         <Route path='/newpassword' component={savePassword} />
                         <Route path='/resetpassword/:email' component={ResetPassword}/>
-                        {/* <Redirect from="*" to="/" component={Login}/> */}
+                        <Redirect from="*" to="/" component={Login}/>
                         </Suspense>
                     </Switch>
                 </Router >
