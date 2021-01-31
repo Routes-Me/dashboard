ed26a654308ca5aa9f855f66ff0f288205089549
Vivian <vtharaka@routesme.com>
2021-01-31 13:19:33 +0300

    GAPI client not initialized

--- src/Redux/Action/GApiAction.js
+++ src/Redux/Action/GApiAction.js
@@ -1,24 +1,10 @@
-
-import gapi from 'gapi';
+const gapi = require('gapi');
 
 var GoogleAuth; // Google Auth object.
-export function initClient() {
-  gapi.client.init({
-      'apiKey': process.env.REACT_API_KEY,
-      'clientId': process.env.REACT_APP_CLIENT_ID,
-      'scope': 'https://www.googleapis.com/auth/androidmanagement',
-      'discoveryDocs': ['https://androidmanagement.googleapis.com/$discovery/rest?version=v1']
-  }).then(function () {
-      GoogleAuth = gapi.auth2.getAuthInstance();
-
-      // Listen for sign-in state changes.
-      GoogleAuth.isSignedIn.listen(updateSigninStatus);
-  });
-}
 
 
+    export function initializeGApi() {
 
-    function authenticate() {
         gapi.client.init({
             'apiKey': process.env.REACT_API_KEY,
             'clientId': process.env.REACT_APP_CLIENT_ID,
@@ -36,7 +22,11 @@ export function initClient() {
             });
     }
 
-  function loadClient() {
+    function updateSigninStatus(){
+      
+    }
+
+  export function loadClient() {
     gapi.client.setApiKey("YOUR_API_KEY");
     return gapi.client.load("https://androidmanagement.googleapis.com/$discovery/rest?version=v1")
         .then(function() { console.log("GAPI client loaded for API"); },
