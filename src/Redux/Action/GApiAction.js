22aff6c5ca07043cee646ad1a55b57acb735da40
vivian tharakan <vtharaka@routesme.com>
2021-02-11 15:55:12 +0300

    loadclient and then list policices

--- src/Redux/Action/GApiAction.js
+++ src/Redux/Action/GApiAction.js
@@ -4,22 +4,21 @@ import { GApiConstants } from '../../constants/GApiConstants';
 export function authenticate() {
   return gapi.auth2.getAuthInstance()
       .signIn({scope: "https://www.googleapis.com/auth/androidmanagement"})
-      .then(function() { console.log("Sign-in successful"); execute();},
+      .then(function() { console.log("Sign-in successful"); },
             function(err) { console.error("Error signing in", err.error.message); alert("Seems like authentication failed!!" + err.error.message) });
 }
 
 function loadClient() {
   gapi.client.setApiKey("AIzaSyBYsa7lx9_fPq0ydSxUt3rstnF_npYD1T4");
   return gapi.client.load("https://androidmanagement.googleapis.com/$discovery/rest?version=v1")
-      .then(function() { console.log("GAPI client loaded for API"); },
+      .then(function() { console.log("GAPI client loaded for API"); execute(); },
             function(err) { alert(`Client Load API => Google Server Response : ${err.error.message}`); 
             console.error("Error loading GAPI client for API", err); });
 }
 
 // Make sure the client is loaded and sign-in is complete before calling this method.
-function execute(dispatch) {
+function execute() {
   
-    dispatch(request());
     gapi.client.androidmanagement.enterprises.policies.list({
       "parent": "enterprises/LC02my9vtl"
     })
