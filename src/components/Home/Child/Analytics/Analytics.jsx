ffffc591990aa06f793ed35eaee370085298e0bc
vivian tharakan <vtharaka@routesme.com>
2021-02-24 11:35:32 +0300

    DVS24022021: Analytics Update, Tracking HubURL

--- src/components/Home/Child/Analytics/Analytics.jsx
+++ src/components/Home/Child/Analytics/Analytics.jsx
@@ -7,16 +7,18 @@ function Analytics(advertisementList) {
 
     const data =  
     [
-        {name: 'Routes',            iOS: 123, Android: 50,  web: 13, Windows: 60,  Mac:20},
+        {name: 'Routes',            iOS: 125, Android: 50,  web: 13, Windows: 61,  Mac:20},
         {name: 'Center Point',      iOS: 28,  Android: 26,  web: 0,  Windows: 19,  Mac:0},
-        {name: 'McDonalds',         iOS: 33,  Android: 20,  web: 2,  Windows: 13,  Mac:0},
+        {name: 'McDonalds',         iOS: 34,  Android: 20,  web: 2,  Windows: 13,  Mac:0},
         {name: 'Zain',              iOS: 27,  Android: 19,  web: 0,  Windows: 12,  Mac:0},
         {name: 'City Center',       iOS: 28,  Android: 9,   web: 0,  Windows: 14,  Mac:4},
-        {name: 'Dominos',           iOS: 16,  Android: 4,   web: 2,  Windows: 11,   Mac:0},
-        {name: 'ooredoo',           iOS: 13,  Android: 4,   web: 3,  Windows: 1,   Mac:11},
+        {name: 'ooredoo',           iOS: 17,  Android: 4,   web: 3,  Windows: 3,   Mac:11},
+        {name: 'Dominos',           iOS: 17,  Android: 4,   web: 2,  Windows: 12,   Mac:0},
         {name: 'Xcite',             iOS: 14,  Android: 8,   web: 0,  Windows: 10,  Mac:0},
         {name: 'Best',              iOS: 10,  Android: 5,   web: 0,  Windows: 5,   Mac:0},
         {name: 'Huawei',            iOS: 9,   Android: 2,   web: 0,  Windows: 5,   Mac:0},
+        {name: 'I save',            iOS: 3,   Android: 0,   web: 0,  Windows: 0,   Mac:0},
+        {name: 'Turkish Grill',     iOS: 2,   Android: 0,   web: 0,  Windows: 0,   Mac:0}, 
         {name: 'KFC',               iOS: 1,   Android: 0,   web: 0,  Windows: 0,   Mac:0} 
     ];
 
