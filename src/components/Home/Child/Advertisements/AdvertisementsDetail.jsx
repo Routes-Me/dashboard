3f02a232e6a9449134300c450f4168ad8546885a
vivian tharakan <vtharaka@routesme.com>
2021-03-04 17:16:02 +0300

    Update on loading animation in progress

--- src/components/Home/Child/Advertisements/AdvertisementsDetail.jsx
+++ src/components/Home/Child/Advertisements/AdvertisementsDetail.jsx
@@ -7,6 +7,9 @@ import { Extras } from './Detail/Extras';
 import ReactPlayer from 'react-player';
 import '../Advertisements/Advertisement.css';
 import { config } from '../../../../constants/config';
+import Launch from '../../../Launch';
+import SolarSystemLoading from 'react-loadingg/lib/SolarSystemLoading';
+import { advertisementsConstants } from '../../../../constants/advertisementConstants';
 
 
 class AdvertisementsDetail extends React.Component {
@@ -52,10 +55,7 @@ class AdvertisementsDetail extends React.Component {
         }
         if (props.UploadedMedia!==undefined && (props.UploadedMedia !== ""))
         {
-            // if (props.UploadedMedia.Type !== state.mediaType) 
-            // {
-                return props.UploadedMedia.Type === 'mp4'? { videoUrl: props.UploadedMedia.Url, mediaType:'mp4', imageUrl:""} : { imageUrl: props.UploadedMedia.Url, mediaType:"jpg" , videoUrl:""};
-            // }
+                return props.UploadedMedia.Type === advertisementsConstants.video? { videoUrl: props.UploadedMedia.Url, mediaType:advertisementsConstants.video, imageUrl:""} : { imageUrl: props.UploadedMedia.Url, mediaType:"jpg" , videoUrl:""};
         }
         if(state.submitBasic)
         {
@@ -119,7 +119,7 @@ class AdvertisementsDetail extends React.Component {
                                 <div className="container row topPanel">
                                     <div className="banner1">
                                     {
-                                        this.state.videoUrl === "" ? videoText :
+                                        this.state.videoUrl === "" ? videoText : this.state.videoUrl ===  advertisementsConstants.loading ?  <SolarSystemLoading color='#234391' style={{ width: '2px', height:'2px'}}/> :
                                             <ReactPlayer
                                                 width='100%'
                                                 height='100%'
@@ -128,7 +128,9 @@ class AdvertisementsDetail extends React.Component {
                                     }
                                     </div>
                                     <div className="banner2">
-                                            {this.state.imageUrl === "" ? imageText : <img className="img-fluid" alt="" src={imageText} />}
+                                            {this.state.imageUrl === "" ? imageText : 
+                                             ? <SolarSystemLoading color='#234391' style={{ width: '2px', height:'2px'}}/> : 
+                                            <img className="img-fluid" alt="" src={imageText} />}
                                     </div>
                                 </div>
                                 <div className="container row bottomPanel">
