c2f326accf520d03a03309190f5dbab269a6a26a
vivian tharakan <vtharaka@routesme.com>
2021-04-23 02:02:43 +0300

    Added GAPi service classes to handle the lib

---
+++ src/util/gapi.js
@@ -0,0 +1,151 @@
+import {inLog, outLog} from './CustomLogging';
+class GapiRequest {
+
+constructor(){
+    this.GAPI = window.gapi;
+    this.loadAuthClient();
+    console.log('Window context loaded onto this')
+}
+
+
+loadAuthClient = () => {
+    this.GAPI.load("client:auth2", function() {
+        window.gapi.auth2.init({client_id: process.env.REACT_APP_GAPI_CLIENTID});
+        console.log('Script loaded !!')
+    });
+}
+
+authorize = () => {
+    inLog('EMM','Authorize Request')
+
+    return this.GAPI.auth2.getAuthInstance()
+    .signIn({scope: "https://www.googleapis.com/auth/androidmanagement"})
+    .then(function() { 
+        outLog('EMM', 'Authorized!!');
+    },
+    function(err) { 
+        outLog('EMM',`Authorization Error: ${err}`); alert("Seems like authentication failed!!" + err.error.message);
+    });
+    
+}
+
+loadClient = () => {
+    inLog('EMM','Loading Client....');
+    return this.GAPI.client.load("https://androidmanagement.googleapis.com/$discovery/rest?version=v1")
+        .then(function() { 
+            outLog('EMM', 'GAPI client loaded for API');
+        },
+        function(err) { alert(`Client Load API ===> Google Server Response : ${err.error.message}`);  outLog('EMM', `Error loading GAPI client for API ${err}`); });
+}
+
+
+
+
+getDevices = () => {
+    inLog('EMM',"Get Devices request");
+    // return window.gapi.client.load("androidmanagement", "v1").then(() => {
+    return this.GAPI.client.androidmanagement.enterprises.devices
+        .list({parent: "enterprises/LC02my9vtl"})
+        .then(
+            function (response) {
+                return response.result.devices;
+            },
+            function (err) {
+                alert(`Devices API => Google Server Response : ${err.error.message}`);
+                outLog("Execute error", err);
+            }
+        );
+    // });
+};
+
+getPolicies = () => {
+    inLog('EMM', 'Get Policies request');
+    return this.GAPI.client.androidmanagement.enterprises.policies
+    .list({
+        parent: 'enterprises/LC02my9vtl'
+    })
+    .then(
+        function (response) {
+            return response.result.policies;
+        },
+        function (err) {
+            outLog('EMM Policies Request Error', err);
+        }
+    )
+}
+
+getPolicy = (policyName) => {
+    return this.GAPI.client.androidmanagement.enterprises.policies.get({
+        "name": policyName
+    })
+    .then(function (response) {
+        return response;
+    },
+    function(err) { 
+        outLog('EMM', `EnrollmentToken Error ${err}`);
+    })
+}
+
+deletePolicy = (name) => {
+    return this.GAPI.client.androidmanagement.enterprises.policies
+    .delete({
+        "name":name
+    })
+    .then(function(response) {
+        console.log('Response ', response);
+    },
+    function (error) {
+        console.log('Error ', error);
+    });
+}
+
+deleteDevice = (name) => {
+    return this.GAPI.client.androidmanagement.enterprises.devices
+        .delete({
+            "name":name
+        })
+        .then(function(response) {
+            console.log('Response ', response);
+        },
+        function (error) {
+            console.log('Error ', error);
+        })
+}
+
+createEnrollmentToken = () =>{
+    return this.GAPI.client.androidmanagement.enterprises.enrollmentTokens.create({
+        "parent": "enterprises/LC02my9vtl",
+        "resource": {
+          "policyName": "enterprises/LC02my9vtl/policies/policyTest"
+        }
+      })
+      .then(function (response) {
+          return response.result
+      },
+      function(err) { 
+          outLog('EMM', `EnrollmentToken Error ${err}`);
+      })
+}
+
+
+createWebToken = () => {
+    inLog('EMM', 'WebToken Request')
+    return this.GAPI.client.androidmanagement.enterprises.webTokens.create({
+        "parent": process.env.REACT_APP_GAPI_ENTERPRICE_ID,
+        "resource": {
+        "parentFrameUrl": "https://localhost:3000/home"
+        }
+    })
+    .then(function(response) {
+        // outLog('EMM',`WebToken Response Success ${response.result.value}`);
+        return response.result.value;
+    },
+    function(err) { 
+        outLog('EMM', `WebToken Error ${err}`);
+    });
+}
+
+}
+
+export default GapiRequest;
+
