462305cec8a32cd2794ff1b1d8c165c33fd29412
vivian tharakan <vtharaka@routesme.com>
2021-08-01 16:01:04 +0300

    Playback & linklog Analytics

--- src/components/Home/Child/Analytics/Analytics.jsx
+++ src/components/Home/Child/Analytics/Analytics.jsx
@@ -6,6 +6,7 @@ import * as AnalyticsAction from '../../../../Redux/Action/AnalyticsAction';
 import { analyticsConstant } from '../../../../constants/analyticsConstants';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { config } from '../../../../constants/config';
 class Analytics extends Component {
 
 
@@ -13,7 +14,8 @@ class Analytics extends Component {
         super(props);
         this.state = {
             startDate: "",
-            endDate: ""
+            endDate: "",
+            entity: config.analytics.qrScans
         }
     }
 
@@ -23,7 +25,7 @@ class Analytics extends Component {
         let strtDate = new Date();
         strtDate.setDate(endDate.getDate() - 21);
         this.setState({ startDate: strtDate, endDate: endDate });
-        this.props.getAddAnalytics(strtDate, endDate)
+        this.props.getAnalytics(this.state.entity, strtDate, endDate)
     }
 
     componentDidUpdate() {
@@ -35,19 +37,32 @@ class Analytics extends Component {
     setDateRange = (update) => {
         this.setState({ startDate: update[0], endDate: update[1] });
         if (update[1] !== null)
-            this.props.getAddAnalytics(update[0], update[1]);
+            this.setState({ startDate: update[0], endDate: update[1] });
+        this.props.getAnalytics(this.state.entity, update[0], update[1]);
+    }
+
+    onChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+        this.props.getAnalytics(event.target.value, this.state.startDate, this.state.endDate);
     }
 
 
     render() {
 
-        this.props.AdvertisementList.data && this.props.AdvertisementList.data.sort(function (a, b) {
-            const aTotal = a.iOS + a.Android + a.web + a.Windows + a.Mac;
-            const bTotal = b.iOS + b.Android + b.web + b.Windows + b.Mac;
-            return bTotal - aTotal;
-        })
+        if (this.props.AdvertisementList.data) {
+            this.state.entity === config.analytics.qrScans ? this.props.AdvertisementList.data.sort(function (a, b) {
+                const aTotal = a.iOS + a.Android + a.web + a.Windows + a.Mac;
+                const bTotal = b.iOS + b.Android + b.web + b.Windows + b.Mac;
+                return bTotal - aTotal;
+            }) : this.props.AdvertisementList.data.sort(function (a, b) {
+                const aTotal = a.Morning + a.Noon + a.Evening + a.Night;
+                const bTotal = b.Morning + b.Noon + b.Evening + b.Night;
+                return bTotal - aTotal;
+            })
+        }
 
-        this.props.AdvertisementList.data && console.log('Render bar chart ', JSON.stringify(this.props.AdvertisementList.data));
+
+        this.props.AdvertisementList.data && console.table(this.props.AdvertisementList.data);
 
         return (
             <div className="vehicles-page" style={{ height: "100vh", width: "100%" }}>
@@ -55,8 +70,20 @@ class Analytics extends Component {
                     <div className="top-part-vehicles-search padding-lr-80">
                         <div className="header-add-butt">
                             <h3>Analytics</h3>
+                            {/* <div className="btn-group">
+                                <button type="button" className="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Action
+                                </button>
+                                <div className="dropdown-menu"> */}
+
+                            {/* </div>
+                            </div> */}
                             {/* <label style={{ float: 'right', fontSize: '14px', fontFamily: 'Roboto' }}>20-dec-2020 - {getCurrentDate()}</label> */}
-                            <div style={{ float: 'right' }}>
+                            <div style={{ float: 'right', display: "flex" }}>
+                                <select value={this.state.entity} className="dateRange" name="entity" onChange={this.onChange}>
+                                    <option key={0} className="dropdown-item" value={0}>Select an category</option>
+                                    {config.analytics.category.map(select => (<option key={select.name} className="dropdown-item" value={select.name}>{select.name}</option>))}
+                                </select>
                                 <DatePicker
                                     className="dateRange"
                                     selectsRange={true}
@@ -76,11 +103,7 @@ class Analytics extends Component {
                             <YAxis />
                             <Tooltip />
                             <Legend />
-                            <Bar dataKey="Android" stackId="a" fill="#a4c639" barSize={30} />
-                            <Bar dataKey="iOS" stackId="a" fill="#a2aaad" barSize={30} />
-                            <Bar dataKey='web' stackId='a' fill='#375a9b' barSize={30} />
-                            <Bar dataKey='Windows' stackId='a' fill='#9b7837' barSize={30} />
-                            <Bar dataKey='Mac' stackId='a' fill='#90a3c4' barSize={30} />
+                            {this.props.AdvertisementList.options.map(option => <Bar dataKey={option.name} stackId="a" fill={option.color} barSize={30} />)}
                         </BarChart>}
                         <br />
                         <br />
@@ -94,7 +117,7 @@ class Analytics extends Component {
                                 </thead>
                                 <tbody>
                                     {this.props.AdvertisementList.data && this.props.AdvertisementList.data.map(x => {
-                                        const total = x.iOS + x.Android + x.web + x.Windows + x.Mac;
+                                        const total = this.state.entity === config.analytics.qrScans ? x.iOS + x.Android + x.web + x.Windows + x.Mac : x.Morning + x.Noon + x.Evening + x.Night;
 
                                         return (
                                             <tr>
@@ -128,7 +151,7 @@ const mapStateToProps = (state) => {
 }
 
 const actionCreators = {
-    getAddAnalytics: AnalyticsAction.getAnalyticsLinkLogs
+    getAnalytics: AnalyticsAction.getAnalyticsLinkLogs
 };
 
 const connectedAnalytics = connect(mapStateToProps, actionCreators)(Analytics);
