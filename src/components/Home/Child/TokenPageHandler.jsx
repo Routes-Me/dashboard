aae031c4461e82d92bac59de2912d2c8f4b54b5b
basharAbdullah <des.basharabdulah@gmail.com>
2023-07-25 12:44:56 +0300

    Firebase file & Emm pagination

--- src/components/Home/Child/TokenPageHandler.jsx
+++ src/components/Home/Child/TokenPageHandler.jsx
@@ -1,18 +1,18 @@
 import React, { memo, useEffect, useState } from 'react'
+import cookie from 'react-cookies';
 
 // let previousToken = "";
 
- export const TokenPageHandler = ({ style, nextPageToken, pageSize, getList, authorize, pageName }) => {
-    console.log(`Values => Page Token : ${nextPageToken} pageSize : ${pageSize} `);
+ export const TokenPageHandler = ({ style, nextPageToken, pageSize, getList, authorize, pageName, data }) => {
     const [pageNumber, setPageNumber] = useState(0)
 
 
     useEffect(() => {
 
-        if(window.localStorage.getItem("pageName") !== pageName) {
-            window.localStorage.clear()
+        if(cookie.load("pageName") !== pageName) {
+            cookie.remove("pageName")
         }
-        window.localStorage.setItem("pageName", pageName)
+        cookie.save("pageName", pageName)
     }, [])
     
 
@@ -21,16 +21,16 @@ import React, { memo, useEffect, useState } from 'react'
         let currentNumber = pageNumber
         currentNumber = currentNumber + 1;
         setPageNumber(currentNumber); 
-        window.localStorage.setItem(currentNumber, nextPageToken)
+        cookie.save(currentNumber, nextPageToken)
         getList(nextPageToken, pageSize, authorize) 
     }
     
     const previousClick = () => {
         let currentNumber = pageNumber;
-        if (currentNumber > 1) {
+        if (currentNumber > 0) {
             currentNumber= currentNumber -1;
             setPageNumber(currentNumber);
-            let previousToken = window.localStorage.getItem(currentNumber)
+            let previousToken = cookie.load(currentNumber)
             getList(previousToken, pageSize, authorize) 
         }
     }
@@ -38,7 +38,7 @@ import React, { memo, useEffect, useState } from 'react'
     return (
         <div className='col-md-12' style={style === 'header' ? { padding: '20px 80px', color: "#979797" } : { padding: '5px', paddingRight: '5px', border: '1px solid lightgrey', height: '40px', color: "#979797" }}>
             <div className='d-flex justify-content-between' style={{ float: "right", fontSize: "18px" }}>
-                <p> Page Size : {pageSize} - {pageNumber}</p>
+                <p> Page Size : {data?.length} - {pageNumber}</p>
                 <span className="glyphicon glyphicon-menu-left" aria-hidden="true" style={{ padding: '5px 0px 0px 20px', cursor: 'pointer', verticalAlign: "middle" }} onClick={ previousClick} />
                 <span className="glyphicon glyphicon-menu-right" aria-hidden="true" style={{ padding: '5px 0px 0px 20px', cursor: 'pointer', verticalAlign: "middle" }} onClick={nextClick} />
             </div>
